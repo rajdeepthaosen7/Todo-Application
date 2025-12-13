c7a9d00c7c574f1f071b69667f81cf935e952a74
rajdeepthaosen7 <rajdeepthaose7@gmail.com>
2025-12-13 12:54:28 +0530

    feat: additions of fields in entity

--- src/main/java/com/cams/todo/dtos/TodoDto.java
+++ src/main/java/com/cams/todo/dtos/TodoDto.java
@@ -2,5 +2,5 @@ package com.cams.todo.dtos;
 
 import java.time.LocalDate;
 
-public record TodoDto (String id, String title, LocalDate dueDate){
+public record TodoDto (String title, String description, LocalDate dueDate){
 }
--- src/main/java/com/cams/todo/entities/TodoEntity.java
+++ src/main/java/com/cams/todo/entities/TodoEntity.java
@@ -1,5 +1,7 @@
 package com.cams.todo.entities;
 
+import com.cams.todo.enums.Priority;
+import com.cams.todo.enums.Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
@@ -18,12 +21,29 @@ public class TodoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "serial_no")
-    String id;
+    Long id;
 
-    @Column(name = "title", nullable = false)
+    @Column(name = "title", nullable = false, length = 255)
     String title;
 
-    @Column(name = "dueDate")
+    @Column(name = "description", nullable = true)
+    String description;
+
+    @Column(name = "due_date")
     LocalDate dueDate;
+
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    LocalDateTime updatedAt;
+
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
+    Priority priority;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    Status status;
 
 }
