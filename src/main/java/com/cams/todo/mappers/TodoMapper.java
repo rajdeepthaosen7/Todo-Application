f4eff3240b5e7d7fd95cb8c257c0adeccb324038
rajdeepthaosen7 <rajdeepthaose7@gmail.com>
2025-12-13 16:22:15 +0530

    fix: pageability

--- src/main/java/com/cams/todo/mappers/TodoMapper.java
+++ src/main/java/com/cams/todo/mappers/TodoMapper.java
@@ -2,15 +2,23 @@ package com.cams.todo.mappers;
 
 import com.cams.todo.dtos.TodoCreateRequest;
 import com.cams.todo.dtos.TodoResponse;
+import com.cams.todo.dtos.TodoUpdateRequest;
 import com.cams.todo.entities.Todo;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "Spring")
+@Mapper(componentModel = "spring")
 public interface TodoMapper {
-    Todo toEntity(TodoCreateRequest todoDto);
-    TodoCreateRequest toDto(Todo todoEntity);
-    TodoResponse toResponse(Todo todoEntity);
-  
 
+    // CREATE
+    Todo toEntity(TodoCreateRequest dto);
+
+    // RESPONSE
+    TodoResponse toResponse(Todo entity);
+
+    // PATCH (partial update)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(TodoUpdateRequest dto, @MappingTarget Todo entity);
 }
-
